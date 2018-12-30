a9d45150fcf399e7739992b1b70bfc76d951c723
Mazin Power <mazinpower@notonthehighstreet.com>
2018-12-30 22:12:02 +0000

    Exec code for creating BMPs

--- main.go
+++ main.go
@@ -2,11 +2,21 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
+
+	"github.com/muzfuz/ray/canvas"
 
 	"github.com/muzfuz/ray/tuple"
 )
 
 func main() {
+	c := canvas.NewCanvas(100, 100)
+	c.WriteAllPixels(canvas.NewColor(0.5, 0.8, 1))
+	err := ioutil.WriteFile("tmp/canvas.bmp", []byte(c.ToPPM()), 0664)
+	if err != nil {
+		fmt.Println(err)
+	}
+
 	p := newProjectile(tuple.NewPoint(0, 1, 0), tuple.NewVector(1, 1, 0).Normalize())
 	e := newEnvironment()
 	t := 0
