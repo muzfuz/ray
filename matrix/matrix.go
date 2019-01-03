9934c5ea0882fa632ee412e592dbbc6856708f51
Mazin Power <mazinpower@notonthehighstreet.com>
2019-01-03 09:49:46 +0000

    Matrix matrix multiplication

--- matrix/matrix.go
+++ matrix/matrix.go
@@ -1,34 +1,57 @@
 package matrix
 
+import (
+	"errors"
+
+	"github.com/muzfuz/ray/float"
+)
+
 // Matrix embodies the data and methods to do
 // matrix based calculations
-type Matrix struct {
-	data [][]float64
-}
+type Matrix [][]float64
 
 // NewMatrix constructs a new matrix
-func NewMatrix(d [][]float64) Matrix {
-	return Matrix{
-		data: d,
+func NewMatrix(rows, columns int) Matrix {
+	d := make([][]float64, rows)
+	for i := range d {
+		d[i] = make([]float64, columns)
 	}
+	return Matrix(d)
 }
 
 // At retrieves the values at a given row / column coordinate
 func (m Matrix) At(r, c int) float64 {
-	return m.data[r][c]
+	return m[r][c]
 }
 
 // Equal will compare two instances and return true if they are the same
 func (m Matrix) Equal(m2 Matrix) bool {
-	if len(m.data) != len(m2.data) || len(m.data[0]) != len(m2.data[0]) {
+	if len(m) != len(m2) || len(m[0]) != len(m2[0]) {
 		return false
 	}
-	for r := range m.data {
-		for c := range m.data[r] {
-			if m.data[r][c] != m2.data[r][c] {
+	for r := range m {
+		for c := range m[r] {
+			if !float.Equal(m[r][c], m2[r][c]) {
 				return false
 			}
 		}
 	}
 	return true
+}
+
+// Multiply will take two matrices and multiply them
+func (m Matrix) Multiply(m2 Matrix) (Matrix, error) {
+	rows, columns := len(m), len(m2[0])
+	if rows != columns {
+		return Matrix{}, errors.New("cannot multiply two matrices with differing row + column sizes")
+	}
+	newMat := NewMatrix(rows, columns)
+	for r := range m {
+		for c := range m2[r] {
+			for i := 0; i < rows; i++ {
+				newMat[r][c] += m[r][i] * m2[i][c]
+			}
+		}
+	}
+	return newMat, nil
 }
--- matrix/matrix_test.go
+++ matrix/matrix_test.go
@@ -9,14 +9,13 @@ import (
 func TestNewMatrix(t *testing.T) {
 	is := assert.New(t)
 
-	m := NewMatrix(
-		[][]float64{
-			{1, 2, 3, 4},
-			{5.5, 6.5, 7.5, 8.5},
-			{9, 10, 11, 12},
-			{13.5, 14.5, 15.5, 16.5},
-		},
-	)
+	m := NewMatrix(4, 4)
+	m = [][]float64{
+		{1, 2, 3, 4},
+		{5.5, 6.5, 7.5, 8.5},
+		{9, 10, 11, 12},
+		{13.5, 14.5, 15.5, 16.5},
+	}
 
 	is.Equal(1.0, m.At(0, 0))
 	is.Equal(4.0, m.At(0, 3))
@@ -26,24 +25,22 @@ func TestNewMatrix(t *testing.T) {
 	is.Equal(13.5, m.At(3, 0))
 	is.Equal(15.5, m.At(3, 2))
 
-	m2 := NewMatrix(
-		[][]float64{
-			{-3, 5},
-			{1, -2},
-		},
-	)
+	m2 := NewMatrix(2, 2)
+	m2 = [][]float64{
+		{-3, 5},
+		{1, -2},
+	}
 	is.Equal(-3.0, m2.At(0, 0))
 	is.Equal(5.0, m2.At(0, 1))
 	is.Equal(1.0, m2.At(1, 0))
 	is.Equal(-2.0, m2.At(1, 1))
 
-	m3 := NewMatrix(
-		[][]float64{
-			{-3, 5, 0},
-			{1, -2, -7},
-			{0, 1, 1},
-		},
-	)
+	m3 := NewMatrix(3, 3)
+	m3 = [][]float64{
+		{-3, 5, 0},
+		{1, -2, -7},
+		{0, 1, 1},
+	}
 	is.Equal(-3.0, m3.At(0, 0))
 	is.Equal(-2.0, m3.At(1, 1))
 	is.Equal(1.0, m3.At(2, 2))
@@ -52,30 +49,60 @@ func TestNewMatrix(t *testing.T) {
 func TestEqual(t *testing.T) {
 	is := assert.New(t)
 
-	a := NewMatrix(
-		[][]float64{
-			{1, 2, 3, 4},
-			{5, 6, 7, 8},
-			{9, 8, 7, 6},
-			{5, 4, 3, 2},
-		},
-	)
-	b := NewMatrix(
-		[][]float64{
-			{1, 2, 3, 4},
-			{5, 6, 7, 8},
-			{9, 8, 7, 6},
-			{5, 4, 3, 2},
-		},
-	)
-	c := NewMatrix(
-		[][]float64{
-			{2, 3, 4, 5},
-			{6, 7, 8, 9},
-			{8, 7, 6, 5},
-			{4, 3, 2, 1},
-		},
-	)
+	a := NewMatrix(4, 4)
+	a = [][]float64{
+		{1, 2, 3, 4},
+		{5, 6, 7, 8},
+		{9, 8, 7, 6},
+		{5, 4, 3, 2},
+	}
+
+	b := NewMatrix(4, 4)
+	b = [][]float64{
+		{1, 2, 3, 4},
+		{5, 6, 7, 8},
+		{9, 8, 7, 6},
+		{5, 4, 3, 2},
+	}
+
+	c := NewMatrix(4, 4)
+	c = [][]float64{
+		{2, 3, 4, 5},
+		{6, 7, 8, 9},
+		{8, 7, 6, 5},
+		{4, 3, 2, 1},
+	}
 	is.True(a.Equal(b))
 	is.False(a.Equal(c))
+}
+
+func TestMultiply(t *testing.T) {
+	is := assert.New(t)
+
+	a := NewMatrix(4, 4)
+	a = [][]float64{
+		{1, 2, 3, 4},
+		{5, 6, 7, 8},
+		{9, 8, 7, 6},
+		{5, 4, 3, 2},
+	}
+
+	b := NewMatrix(4, 4)
+	b = [][]float64{
+		{-2, 1, 2, 3},
+		{3, 2, 1, -1},
+		{4, 3, 6, 5},
+		{1, 2, 7, 8},
+	}
+
+	expected := NewMatrix(4, 4)
+	expected = [][]float64{
+		{20, 22, 50, 48},
+		{44, 54, 114, 108},
+		{40, 58, 110, 102},
+		{16, 26, 46, 42},
+	}
+	mult, err := a.Multiply(b)
+	is.NoError(err)
+	is.True(mult.Equal(expected))
 }
